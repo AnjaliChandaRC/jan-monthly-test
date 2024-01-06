77f78a1d9a4c1efb01457c1af10cffe64398073a
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2024-01-06 12:38:45 +0530

    hero header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,191 @@
+import React, { useEffect, useState } from "react";
+import logoImg from "../assets/images/svg/logo.svg"
+import instaImg from "../assets/images/svg/insta.svg"
+import twitterImg from "../assets/images/svg/twitter.svg"
+import discordImg from "../assets/images/svg/discord.svg"
+const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+  useEffect(() => {
+    if (isMobileMenuOpen) {
+      document.body.classList.add("overflow-hidden-while-loading");
+    } else if (!isMobileMenuOpen) {
+      document.body.classList.remove("overflow-hidden-while-loading");
+    }
+  }, [isMobileMenuOpen]);
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+  return (
+    <nav className="bg_nav">
+      <div className="container xl:max-w-[1140px] h-[100px] sm:h-[80px] px-4 sm:px-8 xl:px-0 mx-auto flex items-center justify-between">
+        <a href="/">
+         <img src={logoImg} alt="logo-img" />
+        </a>
+        <div className="hidden lg:flex items-center">
+          <a
+            href="#home"
+            className="text-base text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:text-[#FF7D01]"
+            onClick={closeMobileMenu}
+          >
+            Home
+          </a>
+          <a
+            href="#about"
+            className="text-base text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:text-[#FF7D01] px-5"
+          >
+            About
+          </a>
+          <a
+            href="#team"
+            className="text-base text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:text-[#FF7D01]"
+          >
+            Team
+          </a>
+          <a
+            href="#roadMap"
+            className="text-base text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:text-[#FF7D01] px-5"
+          >
+            RoadMap
+          </a>
+          <a
+            href="#faqs"
+            className="text-base text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:text-[#FF7D01]"
+          >
+            FAQ 
+          </a>
+
+          <div className="flex gap-3 ps-12 pe-8">
+            <a href="/">
+              <img src={instaImg} alt="insta-img" />
+            </a>
+            <a href="/">
+              <img src={twitterImg} alt="twitter" />
+            </a>
+            <a href="/">
+              <img src={discordImg} alt="discord" />
+              </a>
+            </div>
+                    <div className="flex">
+                <div className="button_border button_border-hover-style">
+                  <span className="block bg-black p-1">
+                    <a href="#connect-wallet">
+                      <button className="common_btn text-base ff_black_sans text-white px-[12px] py-[6px] rounded-[2px]">Connect Wallet</button>
+                    </a>
+                  </span>
+                </div>
+              </div>
+        </div>
+        <div className="lg:hidden custom_index">
+          <button
+            onClick={toggleMobileMenu}
+            className="text-white focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="white"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {isMobileMenuOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu Overlay */}
+      {isMobileMenuOpen && (
+        <div className="lg:hidden fixed top-0 left-0 w-full h-screen bg-black z-50 flex justify-center items-center transition-all duration-300">
+          <div className="text-black py-4 flex justify-center items-center navbar_custom_height">
+            <ul className="flex flex-col items-center animate-fadeIn">
+              <li>
+                <a
+                  href="#home"
+                  className="text-xl text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:opacity-30"
+                  onClick={closeMobileMenu}
+                >
+                  Home
+                </a>
+              </li>
+              <li className="pt-3">
+                <a
+                  href="#about"
+                  className="text-xl text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:opacity-30"
+                  onClick={closeMobileMenu}
+                >
+                  About
+                </a>
+              </li>
+              <li className="pt-3">
+                <a
+                  href="#roadmap"
+                  className="text-xl text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:opacity-30"
+                  onClick={closeMobileMenu}
+                >
+                  Roadmap
+                </a>
+              </li>
+              <li className="pt-3">
+                <a
+                  href="#faqs"
+                  className="text-xl text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:opacity-30"
+                  onClick={closeMobileMenu}
+                >
+                  FAQ
+                </a>
+              </li>
+              <li className="pt-3">
+                <a
+                  href="#team"
+                  className="text-xl text-white ff_M_plus font-normal transition-all duration-300 ease-in-out hover:opacity-30"
+                  onClick={closeMobileMenu}
+                >
+                  Team
+                </a>
+              </li>
+              <div className="flex gap-3 pt-5">
+            <a href="/">
+              <img src={instaImg} alt="insta-img" />
+            </a>
+            <a href="/">
+              <img src={twitterImg} alt="twitter" />
+            </a>
+            <a href="/">
+              <img src={discordImg} alt="discord" />
+              </a>
+            </div>
+               <div className="flex justify-start mt-4">
+                <div className="button_border button_border-hover-style">
+                  <span className="block bg-black p-1">
+                    <a href="#connect-wallet">
+                      <button className="common_btn text-base ff_black_sans text-white px-[12px] py-[6px] rounded-[2px]">Connect Wallet</button>
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </ul>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import Header from "../components/Header"
+import bgLeaves from "../assets/images/png/hero-bg-leaves.png"
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <div className="bg-[url('./assets/images/webp/hero-bg.webp')] bg-center	bg-no-repeat bg-cover	min-h-[120vh] relative flex flex-col z-[2]">
+        <div className='absolute bottom-[30px] lg:bottom-[-170px] -z-0 w-full'>
+          <img src={bgLeaves} alt="hero-bg-leaves" className='w-full' />
+        </div>
+        <Header />
+          <div className='text-center flex justify-end flex-col grow mx-auto h-full px-5'>
+            <h1 className='ff_black_sans text-[40px] sm:text-[64px] text-white'>Trippy Goat</h1>
+            <p className='text-base ff_M_plus text-white font-normal lg:max-w-[558px] mx-auto'>Trippy Goats is a limited collection of 5656 Psychedelic goats, randomly generated from over 40 hand drawn traits. Each goat comes with ownership and commercial usage rights.</p>
+        <div className="flex justify-center pt-8">
+                <div className="button_border button_border-hover-style">
+                  <span className="block bg-black p-1">
+                    <a href="#connect-wallet">
+                      <button className="common_btn text-base ff_black_sans text-white px-[27px] py-[6px] rounded-[2px]">Mint Now</button>
+                    </a>
+                  </span>
+                </div>
+        </div>
+      </div> </div>
+    </>
   )
 }
 
